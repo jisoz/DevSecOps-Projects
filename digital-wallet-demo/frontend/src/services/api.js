71448c2fadb27793937fe8c1d8f177a05bc279c4
jisoz <jadissa127@gmail.com>
2026-01-08 13:12:13 +0400

    update

--- digital-wallet-demo/frontend/src/services/api.js
+++ digital-wallet-demo/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.56.241:8000', // Kong API Gateway
+  baseURL: 'http://192.168.56.241', // Kong API Gateway
   headers: {
     'Content-Type': 'application/json',
   },
